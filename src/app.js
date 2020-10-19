bdb40fde309b2ee196ed41d73b66db0b6b549964
mr. whalen <michael.whalen@sitechhs.com>
2020-10-19 13:20:04 -0400

    sdfgsdfg

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log("connected");
+class Album {
+  constructor(title, artist, url) {
+    this.title = title;
+    this.artist = artist;
+    this.url = url;
+  }
+}
+
+const Tullo = {
+  name: "Tullo",
+  getThis: function () {
+    console.log(this);
+  },
+};
